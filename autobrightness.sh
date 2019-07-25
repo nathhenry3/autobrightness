9e4c01dd0364da23e6ab7d508e7d9524108cd259
nathhenry3 <nathhenry3@gmail.com>
2019-07-26 09:25:07 +1200

    pseudocode

---
+++ autobrightness.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+<<COMMENT1
+Pseudocode:
+
+get system time
+get current brightness = b
+
+if x time
+	b = f(x)
+		Note: f(x) depends on sunset, sunrise, and is a linear piecewise function (triangular) - i.e. increasing 
+		until sun apex, then decreasing. May have higher gradient around sunset/sunrise. Divide b into discrete intervals
+		to match discrete brightness units provided to controller
+if b is manually adjusted
+	end script until 
+		next reboot/login
+
+COMMENT1
+
+           OPTIONS="Hello Quit"
+           select opt in $OPTIONS; do
+               if [ "$opt" = "Quit" ]; then
+                echo done
+                exit
+               elif [ "$opt" = "Hello" ]; then
+                echo Hello World
+               else
+                clear
+                echo bad option
+               fi
+           done
